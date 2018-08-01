5e00a94e30abc076e8dba0be4c6738f421366175
Tetsuya Yokose <gyakutentetsu@gmail.com>
2018-08-01 15:38:33 +0900

    Initial Commit

---
+++ visualMediaJigsaw/visualMediaJigsaw/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  visualMediaJigsaw
+//
+//  Created by 横瀬　哲也 on 2018/08/01.
+//  Copyright © 2018年 Tetsuya Yokose. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
